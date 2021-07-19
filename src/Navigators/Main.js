ebf5a01d5c7fbf46590b2f9885bfc94d4c53c4e0
tuantruongdev <tuantruong.a8k29@gmail.com>
2021-07-19 13:54:48 +0700

    ads

--- src/Navigators/Main.js
+++ src/Navigators/Main.js
@@ -1,14 +1,42 @@
 import React from 'react'
+import {View, TouchableOpacity,Text,StatusBar,Image} from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { IndexExampleContainer } from '@/Containers'
-
+import { Icon } from "react-native-elements";
 const Tab = createBottomTabNavigator()
-
+function Notifications() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+}
 // @refresh reset
 const MainNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={IndexExampleContainer} />
+    <Tab.Navigator
+    tabBarOptions={{
+      activeTintColor: '#09958a',
+    }}
+    >
+      <Tab.Screen options={{
+        tabBarIcon: ({ color, size }) => (
+
+            <Icon name="home" color={color}  size={30} />
+          ),
+         
+        }}
+           name="Home"  component={IndexExampleContainer} />
+      <Tab.Screen
+      options={{
+        tabBarIcon: ({ color, size }) => (
+
+            <Icon name="person" color={color}  size={30} />
+          ),
+         
+        }}
+      
+      name="User" component={Notifications} />
     </Tab.Navigator>
   )
 }
