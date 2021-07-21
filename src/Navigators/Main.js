5a5b52409050c520a5f778857dd34e44128ef251
tuantruongdev <tuantruong.a8k29@gmail.com>
2021-07-21 20:50:11 +0700

    added login func

--- src/Navigators/Main.js
+++ src/Navigators/Main.js
@@ -1,13 +1,64 @@
 import React from 'react'
-import {View, TouchableOpacity,Text,StatusBar,Image} from 'react-native';
+import {View, TouchableOpacity,Text,StatusBar,Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { IndexExampleContainer } from '@/Containers'
 import { Icon } from "react-native-elements";
+import {checkSmartName,login,getTaskFromSVO} from '../api/getTaskFromSVO';
 const Tab = createBottomTabNavigator()
 function Notifications() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notifications!</Text>
+        <TouchableOpacity
+    style={{backgroundColor:"#09958a",borderRadius:5,marginBottom:25}}
+    onPress={async()=>{
+      getTaskFromSVO();
+      return;
+
+      let returnObject= checkSmartName("DTC1854801030076","123").then(
+        returnObj=>{
+         
+          if(returnObj._id!=undefined){
+            console.log(returnObj._id);
+          login(returnObj._id,"Leminh771").then(
+            returnLogin=>{
+            if(returnLogin!="error"){
+              console.log(returnLogin.stt);
+              AsyncStorage.setItem("userInfo",JSON.stringify(returnLogin));
+            console.log("login successfully")
+         //done
+            }else{
+              console.log("passWord err");
+            }
+
+            }
+
+          )
+         
+        }
+          else{
+            console.log("userName err");
+          }
+      
+        }
+        
+
+      );
+     
+     
+    
+    
+    }}
+    >
+      <Text style={{margin:10,color:"white",fontWeight:"700",fontSize:20}}>
+     cac3
+
+      </Text>
+
+    </TouchableOpacity>
+
+
+
     </View>
   );
 }
@@ -26,7 +77,7 @@ const MainNavigator = () => {
           ),
          
         }}
-           name="Home"  component={IndexExampleContainer} />
+           name="Home"  component={Notifications} />
       <Tab.Screen
       options={{
         tabBarIcon: ({ color, size }) => (
@@ -36,7 +87,7 @@ const MainNavigator = () => {
          
         }}
       
-      name="User" component={Notifications} />
+      name="User" component={IndexExampleContainer} />
     </Tab.Navigator>
   )
 }
---
+++ src/api/getTaskFromSVO.js
@@ -0,0 +1,130 @@
+import React ,{Component} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+async function getTaskFromSVO() {
+  
+   
+   
+    let userData =undefined;
+    await  AsyncStorage.removeItem("userInfo");
+  await  AsyncStorage.getItem("userInfo",(err,result)=>{
+       userData= JSON.parse(result);
+      })
+
+    console.log(userData);
+    
+
+    const taskLink="https://api.dhdt.vn/calendar/task";
+    return "";
+    try{
+    var response=await  fetch(taskLink, {
+        method: 'POST',
+        headers: {
+                    'Content-Type':'application/json;charset=UTF-8',
+                    'Accept': 'application/json, text/plain, */*',
+                    'sso_token':'undefined',
+                    'refresh_token': 'undefined',
+                    'if-none-match': '"W/\"b5b-3+NZGVqGPC6cHnb+39bL/VlxSY4\"',
+                    'agent': '{\"brower\":\"SVOapp\",\"version\":\"6.1.3\",\"device_name\":\"ReacNativeApp\",\"unique_device_id\":\"8DA9BD10 - B0D0 - 4808 - AB34 - 4AF30AA044EC\",\"user_agent\":\"Mozilla / 5.0(iPhone; CPU iPhone OS 13_6_1 like Mac OS X) AppleWebKit / 605.1.15(KHTML, like Gecko) Mobile / 15E148\",\"system_name\":\"iOS\",\"device_model\":\"iPhone 7\",\"system_version\":\"13.6.1\"}'
+        
+        },
+        body: JSON.stringify({
+            "type" :"user",
+            "_id" : userName,
+            "passwd":passWord
+            
+        })
+      });
+    }catch(error){}
+      const json = await response.json(); 
+    
+}
+
+
+
+async function login(userName,passWord) {
+    const loginLink="https://api.dhdt.vn/account/login/passwd";
+    console.log("_id="+userName);
+    try{
+    var response=await  fetch(loginLink, {
+        method: 'POST',
+        headers: {
+                    'Content-Type':'application/json;charset=UTF-8',
+                    'Accept': 'application/json, text/plain, */*',
+                    'sso_token':'undefined',
+                    'refresh_token': 'undefined',
+                    'if-none-match': '"W/\"b5b-3+NZGVqGPC6cHnb+39bL/VlxSY4\"',
+                    'agent': '{\"brower\":\"SVOapp\",\"version\":\"6.1.3\",\"device_name\":\"ReacNativeApp\",\"unique_device_id\":\"8DA9BD10 - B0D0 - 4808 - AB34 - 4AF30AA044EC\",\"user_agent\":\"Mozilla / 5.0(iPhone; CPU iPhone OS 13_6_1 like Mac OS X) AppleWebKit / 605.1.15(KHTML, like Gecko) Mobile / 15E148\",\"system_name\":\"iOS\",\"device_model\":\"iPhone 7\",\"system_version\":\"13.6.1\"}'
+        
+        },
+        body: JSON.stringify({
+            "type" :"user",
+            "_id" : userName,
+            "passwd":passWord
+            
+        })
+      });
+     
+      const json = await response.json();
+     // console.log(json);
+      if(Object.keys(json).length>2){
+      let returnLogin= json;
+      
+        return returnLogin;
+
+      }
+
+      return "error";
+
+     
+    }catch(error){
+        console.error(error);
+        return "error";
+    }
+}
+
+async function checkSmartName(smartName) {
+    const smartNameLink="https://api.dhdt.vn/account/login/check-smartname";
+try{
+  var response=await  fetch(smartNameLink, {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json;charset=UTF-8',
+                    'Accept': 'application/json, text/plain, */*',
+                    'sso_token':'undefined',
+                    'refresh_token': 'undefined',
+                    'if-none-match': '"W/\"b5b-3+NZGVqGPC6cHnb+39bL/VlxSY4\"',
+                    'agent': '{\"brower\":\"SVOapp\",\"version\":\"6.1.3\",\"device_name\":\"ReacNativeApp\",\"unique_device_id\":\"8DA9BD10 - B0D0 - 4808 - AB34 - 4AF30AA044EC\",\"user_agent\":\"Mozilla / 5.0(iPhone; CPU iPhone OS 13_6_1 like Mac OS X) AppleWebKit / 605.1.15(KHTML, like Gecko) Mobile / 15E148\",\"system_name\":\"iOS\",\"device_model\":\"iPhone 7\",\"system_version\":\"13.6.1\"}'
+        
+        },
+        body: JSON.stringify({
+            "smartname" : smartName,
+            "acc_type" : "user"
+        })
+      });
+     
+      const json = await response.json();
+     // console.log((Object.keys(json.list_acc).length));
+      //console.log(json.list_acc[0]);
+      if(Object.keys(json.list_acc).length>0){
+       let returnObj=json.list_acc[0];
+      
+        return returnObj;
+
+      }
+
+    //  console.error("error");
+      let returnObj="error";
+      return returnObj;
+  
+
+
+    }catch(error){
+        console.error("error");
+        let returnObj="error";
+        return returnObj;
+    }
+
+}
+
+export  {checkSmartName,login,getTaskFromSVO};
